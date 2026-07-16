8819f4af7abdb596fef62462f8ef5164ae84bc1a
Rutaus <nethiz1@hotmail.com>
2026-07-16 15:04:22 +0300

    fixed characters cards border colors

--- JavaScripts/character-cards.js
+++ JavaScripts/character-cards.js
@@ -8,10 +8,18 @@ let characterHTML = "";
 
 characters.forEach(character => {
 
+    let ownerClass = "";
+
+    if (character.owner) {
+
+        ownerClass = `owner-${character.owner.toLowerCase()}`;
+
+    }
+
     characterHTML += `
         <a
             href="${character.link.replace("./Characters/", "")}"
-            class="character-card">
+            class="character-card ${ownerClass}">
 
             <h2>${character.name}</h2>
 
